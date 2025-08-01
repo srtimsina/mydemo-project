34c8c420d0056acc4690400939850f8eb854dacb
Suryaraj Timsina <devopsuryaraj@gmail.com>
2025-08-01 12:57:45 +0000

    some random chnages

---
+++ README.md
@@ -0,0 +1,2 @@
+# DevOps Portal
+## Ansible
--- apache.sh
+++ apache.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 echo "Installing apache pkg."
-sudo apt update
-sudo apt install apache2 -y
+#sudo apt update
+#sudo apt install apache2 -y
 
-sudo systemctl start apache2
-sudo systemctl status apache2
+if [ -f /var/run/apache2/apache2.pid ];
+then
+	echo "Apache is running"
+else
+	echo "Apache is not running"
+        sudo systemctl start apache2
+        sudo systemctl status apache2
+fi
