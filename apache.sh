370bf3bb740af8bcdb0bc4f36fc1492a8952432d
Suryaraj Timsina <devopsuryaraj@gmail.com>
2025-08-04 12:51:24 +0000

    Install docker packages

--- apache.sh
+++ apache.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Installing apache pkg."
-#sudo apt update
+sudo apt update
 #sudo apt install apache2 -y
 
 if [ -f /var/run/apache2/apache2.pid ];
@@ -15,4 +15,24 @@ fi
 
 sudo apt install openjdk -y
 sudo mkdir /mydata/appdata
+
+touch /mydata/appdata/myfile.log
+
+sudo for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
